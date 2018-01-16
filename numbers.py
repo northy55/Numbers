c7b44c3dab66e1a34aa7d9aa00f49401a43b0f33
Matej Sever <severmatej1@gmail.com>
2018-01-16 08:39:17 +0100

    Function
    Added wellcome message
    Added optional help message
    Changes to print messages

--- numbers.py
+++ numbers.py
@@ -1,11 +1,18 @@
 import random
 
 
+# add help message
+def help_message():
+    print("""
+Your goal is to guess a secret number from a range of numbers. You define the range at the start of the game \n""")
+    start_game()
+
+
 # game function
 def game():
     # User selects range of numbers
-    a = int(input('From: '))
-    b = int(input('To: '))
+    a = int(input('Set lower limit: '))
+    b = int(input('Set upper limit: '))
     # secret number is a random number
     secret_num = random.randint(a, b)
     while True:
@@ -22,7 +29,18 @@ def game():
             print('Wrong! My number is smaller than {}'.format(guess))
             a = guess - 1
 
+
 # Add option to start or Quit the game
-start = input('Press Enter / Return to start, or press Q to quit').lower()
-if start != 'q':
-    game()
+def start_game():
+    start = input('Press Enter / Return to start, or press Q to quit \n').lower()
+    if start != 'q':
+        name = input('What is your name? \n')
+        # Add help message
+        message = input(name + ', welcome to the game of numbers! Do you know how to play? Y/n \n').lower()
+        if message != 'n':
+            game()
+        else:
+            help_message()
+
+
+start_game()
