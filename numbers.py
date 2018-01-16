e4e0e858232ce192d1fb105d76831dab3d887850
Matej Sever <severmatej1@gmail.com>
2018-01-16 08:09:12 +0100

    Functions
    User selects range of numbers and gets tips when his guess in incorrect

--- numbers.py
+++ numbers.py
@@ -1,7 +1,11 @@
 import random
 
+
+# User selects range of numbers
+a = int(input('From: '))
+b = int(input('To: '))
 # secret number is a random number
-secret_num = random.randint(1,10)
+secret_num = random.randint(a, b)
 
 # allow multiple guesses
 while True:
@@ -10,5 +14,11 @@ while True:
     # Print guess result
     if guess == secret_num:
         print('You are right! My number was {}'.format(secret_num))
-    else:
-        print('Sorry, you\'ve missed it')
+        break
+    elif guess < secret_num:
+        print('Wrong! My number is bigger than {}'.format(guess))
+        a = guess + 1
+    elif guess > secret_num:
+        print('Wrong! My number is smaller than {}'.format(guess))
+        a = guess - 1
+
