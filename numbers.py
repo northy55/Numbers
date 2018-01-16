f7d72758f0223e1bdd10b31c6cc851652a9fb809
Matej Sever <severmatej1@gmail.com>
2018-01-16 08:00:13 +0100

    Functionality
    - Secret number is a random number between 1 - 10.
    - User has unlimited guesses

--- numbers.py
+++ numbers.py
@@ -1,11 +1,14 @@
-# Define secret number
-secret_num = 3
+import random
 
-# Get users input
-guess = int(input('Try to guess my secret number: '))
+# secret number is a random number
+secret_num = random.randint(1,10)
 
-# Correct / Wrong answer info
-if guess == secret_num:
-    print('You are right! My number was {}'.format(secret_num))
-else:
-    print('Sorry, you\'ve missed it')
+# allow multiple guesses
+while True:
+    # Get users input
+    guess = int(input('Try to guess my secret number: '))
+    # Print guess result
+    if guess == secret_num:
+        print('You are right! My number was {}'.format(secret_num))
+    else:
+        print('Sorry, you\'ve missed it')
